2f74de27402de72e2189b9a00b407b3085131078
Tyrone Wessels <tyrone.wessels@student.vonknh.nl>
2026-03-16 11:32:24 +0100

    test2

--- noorse-taaltrainer/php/exercise.php
+++
@@ -1,9 +0,0 @@
-<?php
-// exercise.php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $answer = $_POST['answer'];
-    // Demo: altijd goed
-    header('Location: ../pages/score.html');
-    exit();
-}
-?>
